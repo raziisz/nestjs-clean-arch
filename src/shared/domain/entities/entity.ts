64f6fa6d24120bb0827da2e5353f018f22d12f7c
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-28 11:09:07 -0400

    Abstract class entity - unit tests

---
+++ src/shared/domain/entities/entity.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidV4 } from 'uuid'
+
+export abstract class Entity<Props = any>  {
+  public readonly _id: string
+  public readonly props: Props
+
+  constructor(props: Props, id?: string) {
+    this.props = props
+    this._id = id || uuidV4()
+  }
+
+  get id() {
+    return this._id
+  }
+
+  toJSON(): Required<{ id: string } & Props> {
+    return {
+      id: this._id,
+      ...this.props
+    } as Required<{ id: string } & Props>
+  }
+}
+
