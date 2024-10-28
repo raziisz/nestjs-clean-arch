d442ed09a2aaa074eeba2cddf200f63f6a4409cf
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-28 09:55:02 -0400

    Adding UserDataBuilder helper method

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,16 +1,12 @@
-import { faker } from '@faker-js/faker';
 import { UserEntity, UserProps } from '../../user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 
 describe("UserEntity unit test", () => {
   let props: UserProps
   let sut: UserEntity
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
+    props = UserDataBuilder({})
     sut = new UserEntity(props);
   })
   it('Constructor method', () => {
