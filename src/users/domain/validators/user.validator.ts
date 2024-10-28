0c82e31c95dde55f38e4144572ca6aaeb11288d8
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-28 14:29:13 -0400

    Adding validator to user entity

---
+++ src/users/domain/validators/user.validator.ts
@@ -0,0 +1,34 @@
+import { MaxLength, IsString, IsNotEmpty, IsNumber, IsDate, IsOptional } from 'class-validator';
+import { UserProps } from '../entities/user.entity';
+import { ClassValidatorFields } from '@/shared/domain/validators/class-validator-fields';
+
+export class UserRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @MaxLength(100)
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsDate()
+  @IsOptional()
+  createdAt: string;
+
+  constructor({ email, name, password, createdAt }: UserProps) {
+    Object.assign(this, { email, name, password, createdAt });
+  }
+}
+
+export class UserValidator extends ClassValidatorFields<UserRules> {
+  validate(data: UserProps): boolean {
+    return super.validate(new UserRules(data ?? {} as UserProps))
+  }
+}
