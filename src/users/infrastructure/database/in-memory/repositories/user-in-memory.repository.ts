9b6347733fab0fe4b8b478699d790f7d65326eb6
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-29 13:24:31 -0400

    Adding UserInMemoryRepository

---
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -0,0 +1,25 @@
+import { ConflictError } from '@/shared/domain/errors/conflict-error';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { InMemoryRepository } from '@/shared/domain/repositories/in-memory.repository';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+
+export class UserInMemoryRepository
+  extends InMemoryRepository<UserEntity>
+  implements UserRepository
+{
+  findByEmail(email: string): Promise<UserEntity> | null {
+    const entity = this.getEmail(email);
+    if (!entity) throw new NotFoundError(`Entity not found using email ${email}`);
+
+    return entity;
+  }
+  async emailExists(email: string): Promise<void> {
+    const entity = this.getEmail(email);
+    if (entity) throw new ConflictError('Email address already used');
+  }
+
+  private async getEmail(email: string): Promise<UserEntity | null> {
+    return this.items.find(item => item.email === email);
+  }
+}
