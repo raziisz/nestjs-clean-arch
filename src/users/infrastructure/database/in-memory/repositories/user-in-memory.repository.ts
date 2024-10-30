16f7f17552a228b4bc786a016fc9353851165cc8
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-30 14:03:38 -0400

    Refactoring user repository - part 2

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,22 +1,46 @@
 import { ConflictError } from '@/shared/domain/errors/conflict-error';
 import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository';
+import { SortDirection } from '@/shared/domain/repositories/searchable-repository.contracts';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository
+  implements UserRepository.Repository
 {
-  findByEmail(email: string): Promise<UserEntity>  {
+  sortableFields: string[] = ['name', 'createdAt'];
+
+  findByEmail(email: string): Promise<UserEntity> {
     const entity = this.getEmail(email);
-    if (!entity) throw new NotFoundError(`Entity not found using email ${email}`);
+    if (!entity)
+      throw new NotFoundError(`Entity not found using email ${email}`);
 
     return entity;
   }
   async emailExists(email: string): Promise<void> {
     const entity = this.getEmail(email);
     if (entity) throw new ConflictError('Email address already used');
+  }
+
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserRepository.Filter | null,
+  ): Promise<UserEntity[]> {
+    if (!filter) return items;
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<UserEntity[]> {
+    return !sort ? super.applySort(items, 'createdAt', 'desc') : super.applySort(items, sort, sortDir)
+
   }
 
   private async getEmail(email: string): Promise<UserEntity | null> {
