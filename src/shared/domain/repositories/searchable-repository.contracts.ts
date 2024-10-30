95e59911e92d5a2b8a55fae2b54831de03c0aa6d
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-30 08:34:33 -0400

    Adding SearchResult class

--- src/shared/domain/repositories/searchable-repository.contracts.ts
+++ src/shared/domain/repositories/searchable-repository.contracts.ts
@@ -10,6 +10,16 @@ export type SearchProps<Filter = string> = {
   filter?: Filter;
 };
 
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  sort: string | null;
+  sortDir: string | null;
+  filter: Filter | null;
+};
+
 export class SearchParams {
   protected _page: number;
   protected _perPage: number = 15;
@@ -87,9 +97,43 @@ export class SearchParams {
   }
 }
 
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
+  }
+
+  toJSON(forceEntity = false) {
+    return {
+      items: forceEntity ? this.items.map(item => item.toJSON()) : this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    }
+  }
+}
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchInput,
+  SearchParams,
   SearchOutput,
 > extends RepositoryInterface<E> {
   search(props: SearchParams): Promise<SearchOutput>;
