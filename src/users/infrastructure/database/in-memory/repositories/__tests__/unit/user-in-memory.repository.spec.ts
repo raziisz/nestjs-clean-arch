a673670f917b002b5eb28d358056cf59747d11b4
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-30 14:39:40 -0400

    UserInMemoryRepository unit tests - apply filter  methods

--- src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
+++ src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
@@ -38,4 +38,28 @@ describe('UserInMemoryRepository unit tests', () => {
     expect.assertions(0);
     await sut.emailExists('anyemail@anydomain.com');
   });
+
+  it('Should no filter item when filter object is null', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    await sut.insert(entity);
+    const result = await sut.findAll();
+    const spyFilter = jest.spyOn(result, 'filter');
+
+    const itemsFiltered = await sut['applyFilter'](result, null);
+    expect(spyFilter).not.toHaveBeenCalled();
+    expect(itemsFiltered).toStrictEqual(result);
+  });
+
+  it('Should filter name field  using filter param', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'TEST' })),
+      new UserEntity(UserDataBuilder({ name: 'Test' })),
+      new UserEntity(UserDataBuilder({ name: 'FAKE' })),
+    ];
+    const spyFilter = jest.spyOn(items, 'filter');
+
+    const itemsFiltered = await sut['applyFilter'](items, 'TEST');
+    expect(spyFilter).toHaveBeenCalled();
+    expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+  });
 });
