1008b0e025c248027573168d5becdab2866d3cda
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-30 10:45:43 -0400

    SearchableInMemory unit test apply paginate method

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -86,5 +86,26 @@ describe('InMemoryRepository unit tests', () => {
       expect(itemsSorted).toStrictEqual([items[2], items[0], items[1]]);
     });
   });
-  describe('apply paginate method', () => {});
+  describe('apply paginate method', () => {
+    it('Should paginate items', async () => {
+      const items = [
+        new StubEntity({ name: 'a', price: 50 }),
+        new StubEntity({ name: 'b', price: 50 }),
+        new StubEntity({ name: 'c', price: 50 }),
+        new StubEntity({ name: 'd', price: 50 }),
+        new StubEntity({ name: 'e', price: 50 }),
+      ];
+      let itemsPaginated = await sut['applyPaginate'](items, 1, 2);
+      expect(itemsPaginated).toStrictEqual([items[0], items[1]]);
+
+      itemsPaginated = await sut['applyPaginate'](items, 2, 2);
+      expect(itemsPaginated).toStrictEqual([items[2], items[3]]);
+
+      itemsPaginated = await sut['applyPaginate'](items, 3, 2);
+      expect(itemsPaginated).toStrictEqual([items[4]]);
+
+      itemsPaginated = await sut['applyPaginate'](items, 4, 2);
+      expect(itemsPaginated).toStrictEqual([]);
+    });
+  });
 });
