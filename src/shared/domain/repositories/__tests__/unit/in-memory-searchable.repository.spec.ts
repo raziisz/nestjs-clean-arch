af68040d1edf55539b32ca7d9fdd485045700ca1
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-30 09:59:35 -0400

    SearchableInMemory unit test part 1

---
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -0,0 +1,33 @@
+import { Entity } from '@/shared/domain/entities/entity';
+import { InMemorySearchableRepository } from '../../in-memory-searchable.repository';
+
+type StubEntityProps = {
+  name: string;
+  price: number;
+};
+class StubEntity extends Entity<StubEntityProps> {}
+class StubInMemorySearchableRepository extends InMemorySearchableRepository<StubEntity> {
+  sortableFields: string[] = ['name'];
+  protected async applyFilter(
+    items: StubEntity[],
+    filter: string | null,
+  ): Promise<StubEntity[]> {
+    if (!filter) return items;
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+}
+
+describe('InMemoryRepository unit tests', () => {
+  let sut: InMemorySearchableRepository<StubEntity>;
+
+  beforeAll(() => {
+    sut = new StubInMemorySearchableRepository();
+  });
+
+  describe('apply filter method', () => {})
+  describe('apply sort method', () => {})
+  describe('apply paginate method', () => {})
+});
