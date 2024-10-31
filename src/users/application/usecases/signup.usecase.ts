d6fc8461d8c03c937ba4f0fc015107d358bc1ae6
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-31 11:04:02 -0400

    Adding UpdateUserUseCase

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,7 +1,7 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { HashProvider } from '@/shared/application/providers/hash-provider';
-import { UserOutput } from '../dtos/user-output';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
 import { BadRequestError } from '@/shared/application/errors/bad-request-error';
 
@@ -28,7 +28,7 @@ export namespace SignupUseCase {
       );
       await this.userRepository.insert(entity);
 
-      return entity.toJSON()
+      return UserOutputMapper.toOutput(entity);
     }
   }
 }
--- src/users/application/usecases/update-user.usecase.ts
+++ src/users/application/usecases/update-user.usecase.ts
@@ -1,9 +1,11 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
-export namespace GetUserUseCase {
+import { BadRequestError } from '@/shared/application/errors/bad-request-error';
+export namespace UpdateUserUseCase {
   export type Input = {
     id: string;
+    name: string;
   };
 
   export type Output = UserOutput;
@@ -11,7 +13,11 @@ export namespace GetUserUseCase {
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
     async execute(input: Input): Promise<Output> {
+      if (!input.name) throw new BadRequestError('Name not provided');
+
       const entity = await this.userRepository.findById(input.id);
+      entity.update(input.name);
+      await this.userRepository.update(entity);
 
       return UserOutputMapper.toOutput(entity);
     }
