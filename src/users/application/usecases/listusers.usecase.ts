2b6de09c417f2a3c00070846129026a0db6a0077
Luiz Felipe <raziel_libertino@hotmail.com>
2024-10-31 09:57:56 -0400

    Adding ListUsersUseCase - part 2

--- src/users/application/usecases/listusers.usecase.ts
+++ src/users/application/usecases/listusers.usecase.ts
@@ -1,10 +1,15 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
 import { SearchInput } from '@/shared/application/dtos/search-input';
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/pagination-output';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 
 export namespace ListUsersUseCase {
   export type Input = SearchInput;
-  export type Output = void;
+  export type Output = PaginationOutput<UserOutput>;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
@@ -12,7 +17,12 @@ export namespace ListUsersUseCase {
       const params = new UserRepository.SearchParams(input);
       const searchResult = await this.userRepository.search(params);
 
-      return
+      return this.toOutPut(searchResult);
+    }
+
+    private toOutPut(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => UserOutputMapper.toOutput(item));
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
