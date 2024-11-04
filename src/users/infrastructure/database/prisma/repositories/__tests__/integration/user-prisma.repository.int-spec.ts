bff7e3f841c73f71d4d93f9929f3d2aa95ac33b4
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-04 09:33:28 -0400

    Integration tests of delete method

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -84,6 +84,24 @@ describe('UserPrismaRepository integration tests', () => {
     expect(output.name).toBe('new name');
   });
 
+  it('should throws error on delete when entity not found', async () => {
+    expect(() => sut.delete('anyid')).rejects.toThrow(
+      new NotFoundError('UserModel not found using ID anyid'),
+    );
+  });
+
+  it('should delete a entity', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    const newUser = await prismaService.user.create({ data: entity.toJSON() });
+
+    await sut.delete(entity.id);
+
+    const output = await prismaService.user.findUnique({
+      where: { id: entity.id },
+    });
+    expect(output).toBeNull();
+  });
+
   describe('search method tests', () => {
     it('should apply only pagination when the other params are null', async () => {
       const createdAt = new Date();
