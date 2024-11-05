5164121d39f9673cfbe8a2072ba3c22f218d6931
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-05 15:45:48 -0400

    Config ValidationPipe and DTOs

--- src/users/infrastructure/dtos/update-password.dto.ts
+++ src/users/infrastructure/dtos/update-password.dto.ts
@@ -1,8 +1,14 @@
 import { UpdatePasswordUserUseCase } from '@/users/application/usecases/update-password-user.usecase';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePasswordDto
   implements Omit<UpdatePasswordUserUseCase.Input, 'id'>
 {
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   oldPassword: string;
 }
