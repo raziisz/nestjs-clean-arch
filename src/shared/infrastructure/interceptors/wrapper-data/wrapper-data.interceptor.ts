07fbf6ac507c5be9d3e15ade5c5f1ea9018ce726
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-05 15:34:24 -0400

    WrapperDataInterceptor unit tests

--- src/shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor.ts
+++ src/shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor.ts
@@ -11,7 +11,6 @@ export class WrapperDataInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map(body => {
-
         return !body || 'meta' in body ? body :  { data: body }
       }),
     );
