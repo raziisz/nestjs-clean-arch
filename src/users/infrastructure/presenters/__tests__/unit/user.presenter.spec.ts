f0e71323100bb8aab30a87503d3abb73e962ffa0
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-05 09:31:05 -0400

    UserPresenter unit tests - part 2

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,5 +1,5 @@
-import { UserOutput } from '@/users/application/dtos/user-output';
 import { UserPresenter } from '../../user.presenter';
+import { instanceToPlain } from 'class-transformer';
 
 describe('UserPresenter unit tests', () => {
   const createdAt = new Date();
@@ -10,14 +10,28 @@ describe('UserPresenter unit tests', () => {
     password: '1234',
     createdAt,
   };
+  let sut: UserPresenter;
+
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
 
   describe('constructor', () => {
     it('should be defined', () => {
-      const sut = new UserPresenter(props);
       expect(sut.id).toEqual(props.id);
       expect(sut.name).toEqual(props.name);
       expect(sut.email).toEqual(props.email);
       expect(sut.createdAt).toEqual(props.createdAt);
     });
-  })
+  });
+
+  it('should presenter data', () => {
+    const output = instanceToPlain(sut);
+    expect(output).toStrictEqual({
+      id: '878f0e98-f9e4-4c90-8ae3-b840f539808f',
+      name: 'Jhon Doe',
+      email: 'a@a.com',
+      createdAt: createdAt.toISOString(),
+    });
+  });
 });
