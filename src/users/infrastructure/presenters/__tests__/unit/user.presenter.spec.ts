0b2316fb984a8e5c69caae3850a2ae271ce62fd9
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-05 14:21:09 -0400

    UserCollectionPresenter unit tests

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,4 +1,5 @@
-import { UserPresenter } from '../../user.presenter';
+import { PaginationPresenter } from '@/shared/infrastructure/presenters/pagination.presenter';
+import { UserCollectionPresenter, UserPresenter } from '../../user.presenter';
 import { instanceToPlain } from 'class-transformer';
 
 describe('UserPresenter unit tests', () => {
@@ -32,6 +33,65 @@ describe('UserPresenter unit tests', () => {
       name: 'Jhon Doe',
       email: 'a@a.com',
       createdAt: createdAt.toISOString(),
+    });
+  });
+});
+
+describe('UserCollectionPresenter unit tests', () => {
+  const createdAt = new Date();
+  let props = {
+    id: '878f0e98-f9e4-4c90-8ae3-b840f539808f',
+    name: 'Jhon Doe',
+    email: 'a@a.com',
+    password: '1234',
+    createdAt,
+  };
+  describe('constructor', () => {
+    it('should be defined', () => {
+      const sut = new UserCollectionPresenter({
+        items: [props],
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 1,
+        total: 1,
+      });
+      expect(sut.meta).toBeInstanceOf(PaginationPresenter);
+      expect(sut.meta).toStrictEqual(
+        new PaginationPresenter({
+          currentPage: 1,
+          perPage: 2,
+          lastPage: 1,
+          total: 1,
+        }),
+      );
+      expect(sut.data).toStrictEqual([new UserPresenter(props)]);
+    });
+  });
+
+  it('should presenter data', () => {
+    const sut = new UserCollectionPresenter({
+      items: [props],
+      currentPage: 1,
+      perPage: 2,
+      lastPage: 1,
+      total: 1,
+    });
+    const output = instanceToPlain(sut);
+    expect(output).toStrictEqual({
+      data: [
+        {
+          id: '878f0e98-f9e4-4c90-8ae3-b840f539808f',
+          name: 'Jhon Doe',
+          email: 'a@a.com',
+          createdAt: createdAt.toISOString(),
+        },
+      ],
+      meta: {
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 1,
+        total: 1,
+      },
     });
   });
 });
