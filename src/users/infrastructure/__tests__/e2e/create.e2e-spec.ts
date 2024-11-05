11e4adcefe5b0ce4f7df25d4c017aae243195db3
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-05 16:07:14 -0400

    Adding ConflictErrorFilter

--- src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -11,6 +11,8 @@ import request from 'supertest';
 import { UsersController } from '../../users.controller';
 import { instanceToPlain } from 'class-transformer';
 import { applyGlobalConfig } from '@/global-config';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 
 describe('UsersController', () => {
   let app: INestApplication;
@@ -131,5 +133,17 @@ describe('UsersController', () => {
       expect(res.body.error).toBe('Unprocessable Entity');
       expect(res.body.message).toEqual(['property xpto should not exist']);
     });
+  });
+
+  it('should return a error with 409 code when the email is duplicated', async () => {
+    const entity = new UserEntity(UserDataBuilder({ ...signupDto, email: 'a@a.com' }));
+    await repository.insert(entity);
+
+    const res = await request(app.getHttpServer())
+      .post('/users')
+      .send(signupDto)
+      .expect(409);
+
+    console.log(res.body)
   });
 });
