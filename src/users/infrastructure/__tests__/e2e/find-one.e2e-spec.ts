4c49e537817419ad6a8753f4695600eb876a18f0
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-07 13:24:02 -0400

    UsersController e2e tests - findOne method

---
+++ src/users/infrastructure/__tests__/e2e/find-one.e2e-spec.ts
@@ -0,0 +1,69 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module';
+import { UsersModule } from '../../users.module';
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import request from 'supertest';
+import { UsersController } from '../../users.controller';
+import { instanceToPlain } from 'class-transformer';
+import { applyGlobalConfig } from '@/global-config';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+
+describe('UsersController e2e tests', () => {
+  let app: INestApplication;
+  let module: TestingModule;
+  let repository: UserRepository.Repository;
+  const prismaService = new PrismaClient();
+  let entity: UserEntity;
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [
+        EnvConfigModule,
+        UsersModule,
+        DatabaseModule.forTest(prismaService),
+      ],
+    }).compile();
+
+    app = module.createNestApplication();
+    applyGlobalConfig(app);
+    await app.init();
+    repository = module.get<UserRepository.Repository>('UserRepository');
+  });
+
+  beforeEach(async () => {
+    await prismaService.user.deleteMany();
+    entity = new UserEntity(UserDataBuilder({}));
+    await repository.insert(entity);
+  });
+
+  describe('GET /users/:id', () => {
+    it('should get a user', async () => {
+      const res = await request(app.getHttpServer())
+        .get(`/users/${entity.id}`)
+        .expect(HttpStatus.OK);
+
+      const presenter = UsersController.userToResponse(entity.toJSON());
+      const serialized = instanceToPlain(presenter);
+
+      expect(res.body.data).toStrictEqual(serialized);
+    });
+
+    it('should return a error with 404 code when throw NotFoundError with invalid id', async () => {
+      const res = await request(app.getHttpServer())
+        .get(`/users/anyid`)
+        .expect(HttpStatus.NOT_FOUND)
+        .expect({
+          statusCode: HttpStatus.NOT_FOUND,
+          error: 'Not Found',
+          message: 'UserModel not found using ID anyid',
+        });
+
+    });
+  });
+});
