3495a39c7c3b391e8b23f5087b7a1d2e7df384da
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-08 09:55:36 -0400

    Refactoring login route

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -55,20 +55,24 @@ describe('UsersController', () => {
   });
 
   it('should authenticate a user', async () => {
-    const output: SigninUseCase.Output = props;
+    const output = 'fake_token';
     const mockSigninUseCase = {
       execute: jest.fn().mockReturnValue(Promise.resolve(output)),
     };
+
+    const mockAuthService = {
+      generateJwt: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
 
     sut['signinUseCase'] = mockSigninUseCase as any;
     const input: SigninDto = {
       email: 'a@a.com',
       password: '1234',
     };
+    sut['authService'] = mockAuthService as any;
 
-    const presenter = await sut.login(input);
-    expect(presenter).toBeInstanceOf(UserPresenter);
-    expect(presenter).toStrictEqual(new UserPresenter(output));
+    const result = await sut.login(input);
+    expect(result).toEqual(output);
     expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input);
   });
   it('should update a user', async () => {
--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -25,7 +25,11 @@ import { SigninDto } from './dtos/signin.dto';
 import { ListUsersDto } from './dtos/list-users.dto';
 import { UpdatePasswordDto } from './dtos/update-password.dto';
 import { UserOutput } from '../application/dtos/user-output';
-import { UserCollectionPresenter, UserPresenter } from './presenters/user.presenter';
+import {
+  UserCollectionPresenter,
+  UserPresenter,
+} from './presenters/user.presenter';
+import { AuthService } from '@/auth/infrastructure/auth.service';
 
 @Controller('users')
 export class UsersController {
@@ -50,6 +54,9 @@ export class UsersController {
   @Inject(DeleteUserUseCase.UseCase)
   private deleteUserUseCase: DeleteUserUseCase.UseCase;
 
+  @Inject(AuthService)
+  private authService: AuthService;
+
   static userToResponse(output: UserOutput) {
     return new UserPresenter(output);
   }
@@ -68,7 +75,7 @@ export class UsersController {
   @Post('login')
   async login(@Body() signinDto: SigninDto) {
     const output = await this.signinUseCase.execute(signinDto);
-    return UsersController.userToResponse(output);
+    return this.authService.generateJwt(output.id);
   }
 
   @Get()
