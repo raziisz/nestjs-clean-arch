9b0a6c5047b73f03b0278aa06c9356530b272c9c
Luiz Felipe <raziel_libertino@hotmail.com>
2024-11-11 14:34:16 -0400

    Refactoring the DTOs with the documentation

--- src/users/infrastructure/dtos/update-password.dto.ts
+++ src/users/infrastructure/dtos/update-password.dto.ts
@@ -1,13 +1,16 @@
 import { UpdatePasswordUserUseCase } from '@/users/application/usecases/update-password-user.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePasswordDto
   implements Omit<UpdatePasswordUserUseCase.Input, 'id'>
 {
+  @ApiProperty({ description: 'Nova senha do usuário' })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ description: 'Senha atual do usuário' })
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
